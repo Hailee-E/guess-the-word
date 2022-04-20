b004fec59e287c20c7a0f69436f9cc6bb6caf0ce
Hailee E <herikson24@hotmail.com>
2022-04-19 20:10:59 -0700

    Add Display Word commit

--- js/script.js
+++ js/script.js
@@ -1,55 +1,90 @@
 const guessedLettersElement = document.querySelector(".guessed-letters");
-const button = document.querySelector(".guess");
-const textInput = document.querySelector(".letter")
+const guessLetterButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuessesElement = document.querySelector(".remaining");
 const remainingGuessesSpan = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
-const playAgain = document.querySelector(".play-again");
+const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
 const guessedLetters = [];
 
 const placeholder = function (word) {
-    const placeholderLetters = [];
-    for (const letter of word) {
+  const placeholderLetters = [];
+  for (const letter of word) {
     console.log(letter);
     placeholderLetters.push("●");
-    }
-    wordInProgress.innerText = placeholderLetters.join("");
+  }
+  wordInProgress.innerText = placeholderLetters.join("");
 };
+
 placeholder(word);
 
-button.addEventListener("click", function (e) {
-    e.preventDefault();
-    message.innerText = "";
-    const guess = textInput.value;
-    const goodGuess = validateInput(guess);
-    if (goodGuess) {
-        makeGuess(guess);
-    }
-    textInput.value = "";
+guessLetterButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  message.innerText = "";
+  const guess = letterInput.value;
+  const goodGuess = validateInput(guess);
+
+  if (goodGuess) {
+    makeGuess(guess);
+  }
+  letterInput.value = "";
 });
 
 const validateInput = function (input) {
-    const acceptedLetter = /[a-zA-Z]/;
-    if (input.length === 0) {
-        message.innerText = "Please enter a letter.";
-    } else if (input.length > 1) {
-        message.innerText = "Please enter a single letter";
-    } else if (!input.match(acceptedLetter)) {
-        message.innerText = "Please enter a letter from A to Z.";
-    } else {
-        return input;
-    }
+  const acceptedLetter = /[a-zA-Z]/;
+  if (input.length === 0) {
+    message.innerText = "Please enter a letter.";
+  } else if (input.length > 1) {
+    message.innerText = "Please enter a single letter.";
+  } else if (!input.match(acceptedLetter)) {
+    message.innerText = "Please enter a letter from A to Z.";
+  } else {
+    return input;
+  }
 };
 
 const makeGuess = function (guess) {
-    guess = guess.toUpperCase ();
-    if (guessedLetters.includes(guess)) {
-        message.innerText = "You already guessed that letter, silly. Try again.";
+  guess = guess.toUpperCase();
+  if (guessedLetters.includes(guess)) {
+    message.innerText = "You already guessed that letter, silly. Try again.";
+  } else {
+    guessedLetters.push(guess);
+    console.log(guessedLetters);
+    showGuessedLetters();
+    updateWordInProgress(guessedLetters);
+  }
+};
+
+const showGuessedLetters = function () {
+  guessedLettersElement.innerHTML = "";
+  for (const letter of guessedLetters) {
+    const li = document.createElement("li");
+    li.innerText = letter;
+    guessedLettersElement.append(li);
+  }
+};
+
+const updateWordInProgress = function (guessedLetters) {
+  const wordUpper = word.toUpperCase();
+  const wordArray = wordUpper.split("");
+  const revealWord = [];
+  for (const letter of wordArray) {
+    if (guessedLetters.includes(letter)) {
+      revealWord.push(letter.toUpperCase());
     } else {
-        guessedLetters.push(guess);
-        console.log(guessedLetter);
+      revealWord.push("●");
     }
+  }
+  wordInProgress.innerText = revealWord.join("");
+  checkIfWin();
+};
+
+const checkIfWin = function () {
+  if (word.toUpperCase() === wordInProgress.innerText) {
+    message.classList.add("win");
+    message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
+  }
 };
